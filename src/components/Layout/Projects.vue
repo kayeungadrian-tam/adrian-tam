b228da4261df9ff70fd9db17063f1c7dcd8db891
AIF-adrian <tamkayeung.adrian@gmail.com>
2022-06-16 00:37:19 +0900

    add: project one

--- src/components/Layout/Projects.vue
+++ src/components/Layout/Projects.vue
@@ -3,24 +3,45 @@
     <TitleComponent :title="title" :description="description" />
     <el-row class="row-bg" justify="space-evenly">
       <el-col :span="16">
-        <el-carousel height="70vh" indicator-position="">
+        <el-carousel height="70vh" indicator-position="" :interval="6000">
           <el-carousel-item v-for="(item, index) in projectList" :key="item">
             <div class="container">
-              <el-skeleton :rows="5" />
-              {{ index }}
+              <h3>
+                {{ index + 1 }} -
+                {{ item.description }}
+              </h3>
+              <el-image
+                style="width: 400px; height: 280px"
+                :src="item.img"
+                fit="contain"
+              />
+              <div class="tag">
+                <el-tag
+                  v-for="tag in item.tags"
+                  :class="tag"
+                  :key="tag"
+                  :type="tag"
+                  effect="dark"
+                >
+                  {{ tag }}
+                </el-tag>
+              </div>
             </div>
             <div class="detail">
               <el-row :gutter="20">
                 <el-col :span="4"><div class="" /> </el-col>
                 <el-col :span="16">
-                  <el-icon
-                    @click="showPage(item.link)"
-                    class="icon-down"
-                    style="vertical-align: middle"
-                    ><DArrowLeft class="down"
-                  /></el-icon>
+                  <div id="container" style="padding-top: 20px">
+                    <button class="learn-more" @click="showPage(item.link)">
+                      <span class="circle" aria-hidden="true">
+                        <span class="icon arrow" style="rotate: 90"></span>
+                      </span>
+                      <span class="button-text">Learn More</span>
+                    </button>
+                  </div>
                 </el-col>
-                <el-col :span="4"></el-col>
+
+                <el-col :span="4"> </el-col>
               </el-row>
             </div>
           </el-carousel-item>
@@ -38,7 +59,60 @@
   </div>
 </template>
 
+<style lang="scss" scoped>
+@import "../../assets/styles/scss/button.scss";
+</style>
+
 <style scoped>
+.github {
+  font-size: 3rem;
+  color: #fff;
+}
+
+.github:hover {
+  font-size: 3rem;
+  color: ;
+}
+
+.ml-2 {
+  margin-left: 2px;
+  color: #fff;
+}
+
+.el-tag {
+  border: 0px solid #fff;
+}
+
+.Python {
+  background-color: #2a2a72;
+  background-image: linear-gradient(315deg, #2a2a72 0%, #009ffd 74%);
+}
+
+.OpenCV {
+  background-color: #a4508b;
+  background-image: linear-gradient(326deg, #a4508b 0%, #5f0a87 74%);
+}
+
+.PysimpleGUI {
+  background-color: #63d471;
+  background-image: linear-gradient(315deg, #63d471 0%, #233329 74%);
+}
+
+.Yolov3 {
+  background-color: #f9484a;
+  background-image: linear-gradient(315deg, #f9484a 0%, #fbd72b 74%);
+}
+
+.tag {
+  position: absolute;
+  display: flex;
+  justify-content: space-between;
+  gap: 3px;
+  align-items: center;
+  bottom: 10px;
+  margin-left: 2px;
+}
+
 .down {
   transform: rotate(-90deg);
   font-size: 20px;
@@ -62,12 +136,12 @@
   background-color: #2d3436;
   background-image: linear-gradient(315deg, #000000 15%, #2d3436 64%);
   color: white;
+  height: 100vh;
 }
 
 .page2 {
   padding: 0% 10px 3% 10px;
   background-color: whitesmoke;
-  height: 200vh;
 }
 
 .row-bg {
@@ -113,6 +187,7 @@
   flex-direction: column;
   height: 84%;
   backdrop-filter: blur(1px);
+  align-items: center;
 }
 </style>
 
@@ -147,14 +222,17 @@ export default {
         {
           no: 1,
           name: "Project 1",
-          description: "This is project 1",
+          description: "Image Processing GUI",
           link: "project-one",
+          img: require("../../assets/projects/thumbnails/image_processing_gui.png"),
+          tags: ["Python", "OpenCV", "PysimpleGUI", "Yolov3"],
         },
         {
           no: 2,
           name: "Project 2",
           description: "This is project 2",
           link: "home",
+          img: require("../../assets/logo.png"),
         },
       ],
     };
--- src/components/Projects/ProjectOne.vue
+++ src/components/Projects/ProjectOne.vue
@@ -5,44 +5,85 @@
         <div class="topboder">
           <el-row>
             <el-col :span="8">
-              <div class="topbox">HERE</div>
+              <div class="topbox">Project 1</div>
             </el-col>
-            <el-col :span="8">HERE</el-col>
-            <el-col :span="8"><div class="top-right">Project 1</div> </el-col>
+            <el-col :span="4"></el-col>
+            <el-col :span="12"
+              ><div class="top-right">Image Processing GUI</div>
+            </el-col>
           </el-row>
         </div>
       </el-header>
     </el-container>
+
+    <img src="../../assets/projects/thumbnails/image_processing_gui.png" />
     <div class="main">
-      <el-row>
-        <el-col :span="12">HERE</el-col>
-        <el-col :span="12"><div class="">Project 1</div> </el-col>
-      </el-row>
+      <h1>Problem Statement</h1>
+      <p>
+        Image augmentation and processing were needed for a object detection
+        task. The detection task was to detect a specific object among a set of
+        similar images. Image processing was perfomed on a pixel level and it
+        was repetitve and time consuming.
+      </p>
+      <h1>Solution</h1>
+      <p>
+        A home-made-from-scratch image processing GUI was developed in python to
+        ease the process. It was designed to augment the image and perfom object
+        detection on the modified image. It also provides a color-seperation
+        function to identify dominant colors in the image.
+      </p>
+      <h1>In the future...</h1>
+      <p>
+        This GUI serves as a platform for real time image processing tester for
+        object detection tasks. Given enough object detection models, it will be
+        possible to test the performance of the models on an augmented image.
+      </p>
     </div>
   </div>
 </template>
 
 <style scoped>
+img {
+  width: 500px;
+  height: auto;
+  padding: 20px;
+}
+
+p {
+  font-size: 16px;
+  line-height: 26px;
+  text-indent: 20px;
+  margin: 0;
+  padding-bottom: 10px;
+}
+
 .main {
   padding: 20px;
-  height: 100vh;
+  text-align: left;
 }
 
 h1 {
-  font-size: 2rem;
-  font-weight: bold;
-  color: #2b4162;
-  width: 100%;
+  align-self: left;
+  color: #7c795d;
+  font-family: "Trocchi", serif;
+  font-size: 1.8rem;
+  font-weight: weight;
+  line-height: 48px;
+  margin: 0;
+  text-shadow: 1px 1px 0 #fff;
+  text-decoration: underline 1px;
 }
-
 .common-layout {
   padding: 1% 20px 3% 20px;
   margin: 0 20px 0 20px;
+  background-color: #d9d9d9;
+  background-image: linear-gradient(315deg, #d9d9d9 0%, #f6f2f2 74%);
+  min-height: 100vh;
 }
 
 .top-right {
   right: 0;
-  font-size: 2rem;
+  font-size: 1.5rem;
   line-height: 50px;
   text-align: right;
   padding-right: 20%;
@@ -58,6 +99,7 @@ h1 {
   background-color: #d3d3d3;
   background-image: linear-gradient(315deg, #d3d3d3 0%, #eccc68 74%);
   height: 50px;
+  line-height: 50px;
 }
 </style>
 
